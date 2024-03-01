3ba241470ce0f3a3ec2453664c84de85881584d8
project-ijon <chrisjhonijon@gmail.com>
2024-03-01 12:32:01 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module tugas1-biodata
---
+++ helpers/init.go
@@ -0,0 +1,18 @@
+package helpers
+
+func Init() []student {
+	data_student := []student{
+		{},
+		{nama: "Reza Rahardian", alamat: "jl Perintis kemerdekaan no 1", pekerjaan: "Polisi", alasan: "Karna sudah takdir tuhan"},
+		{nama: "Ahmad Sukar", alamat: "jl Perintis kemerdekaan no 2", pekerjaan: "Tentara", alasan: " Untuk mengisi waktu luang"},
+		{nama: "Cinta Maharani", alamat: "jl Perintis kemerdekaan no 3", pekerjaan: "Dokter", alasan: "Untuk menambah ilmu"},
+		{nama: "Andika Pratama", alamat: "jl Perintis kemerdekaan no 4", pekerjaan: "Programmer", alasan: "Untuk pekerjaan"},
+		{nama: "Steven Varel", alamat: "jl Perintis kemerdekaan no 5", pekerjaan: "Staff alfamart", alasan: "Supaya jago ngoding"},
+		{nama: "Andre Taulani", alamat: "jl Perintis kemerdekaan no 6", pekerjaan: "Staff Indomart", alasan: "Agar tambah pintar"},
+		{nama: "Muh. Zulfikar", alamat: "jl Perintis kemerdekaan no 7", pekerjaan: "Sopir Taxi", alasan: "Mencari Pengalaman baru"},
+		{nama: "Sri Wahyuni", alamat: "jl Perintis kemerdekaan no 8", pekerjaan: "Perawat", alasan: "Mau ganti karir jadi programmer"},
+		{nama: "Ayu Lestari", alamat: "jl Perintis kemerdekaan no 9", pekerjaan: "Pramugari", alasan: "untuk menambah ilmu dalam bidang perogrammer"},
+		{nama: "Siti Vivian", alamat: "jl Perintis kemerdekaan no 10", pekerjaan: "Polwan", alasan: "Biar bisa buat aplikasi"},
+	}
+	return data_student
+}
